45b635485ac3be7101573589297326d8d61430d7
Ather <atherrehan@live.com>
2025-08-05 13:56:08 +0400

    Chat simple and chat streaming

--- SemanticKernel.SKKernel/Program.cs
+++ SemanticKernel.SKKernel/Program.cs
@@ -1,38 +1,28 @@
-﻿using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
+﻿using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SemanticKernel.SKKernel;
 
 var openAIKey = Environment.GetEnvironmentVariable("SemanticKernelOpenAIKey", EnvironmentVariableTarget.User);
+
 var azureRegion = Environment.GetEnvironmentVariable("SemanticKernelAzureRegion", EnvironmentVariableTarget.User);
+
 var azureKey = Environment.GetEnvironmentVariable("SemanticKernelAzureKey", EnvironmentVariableTarget.User);
 
+var modelIdOpenAI = "gpt-4o-mini-2024-07-18";
 
-////Using OpenAPI Key////
-///
-Kernel openAIKernel = Kernel.CreateBuilder()
-    .AddOpenAIChatCompletion("gpt-4o-mini-2024-07-18", $"{openAIKey}")
-    .Build();
-
-////Using Azure OpenAI Key////
-//Kernel azureKernel = Kernel.CreateBuilder()
-//    .AddAzureOpenAIChatCompletion
-//    (
-//    "gpt-4o-mini",
-//    $"{azureRegion}",
-//    $"{azureKey}")
-//    .Build();
-
-
-var options = new OpenAIPromptExecutionSettings
+var options = new OpenAIPromptExecutionSettings // Set as many properties you want
 {
-    MaxTokens = 10, // Set the maximum number of tokens to return
-    Temperature = 0.7f // Set the temperature for response variability
-};// Set as many properties you want
+    MaxTokens = 50,                             // Set the maximum number of tokens to return
+    Temperature = 0.7f                          // Set the temperature for response variability
+};
+
+var prompt = "What is Semantic Kernel, describe briefly"; // What whaterver you want LLM to return
 
 
-var prompt = "What is Semantic Kernel, describe briefly";//What whaterver you want LLM to return
+SKChat openAIChat = new SKChat(modelIdOpenAI, openAIKey ?? "", prompt, "");
+//await openAIChat.OpenAIChat(options);
+//await openAIChat.AzureAIChat(options);
+await openAIChat.OpenAIChatStreaming(options);
+//await openAIChat.AzureAIChatStreaming(options);
 
-var result = await openAIKernel.InvokePromptAsync(prompt, new KernelArguments(options)); //openaikernel
 
-//var result = await azureKernel.InvokePromptAsync(prompt); // azurekernel
 
-Console.WriteLine(result);
---
+++ SemanticKernel.SKKernel/SKChat.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace SemanticKernel.SKKernel
+{
+    public class SKChat
+    {
+        private readonly string _model;
+        private readonly string _key;
+        private readonly string _prompt;
+        private readonly string _region;
+        public SKChat(string model, string key, string prompt, string region)
+        {
+            _key = key;
+            _model = model;
+            _prompt = prompt;
+            _region = region;
+        }
+        public async Task OpenAIChat(OpenAIPromptExecutionSettings options)
+        {
+            Kernel openAIKernel = Kernel.CreateBuilder()
+                .AddOpenAIChatCompletion(_model, _key)
+                .Build();
+
+            var result = await openAIKernel.InvokePromptAsync(_prompt, new KernelArguments(options));
+
+            Console.WriteLine(result);
+        }
+        public async Task AzureAIChat(OpenAIPromptExecutionSettings options)
+        {
+            Kernel azureKernel = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(_model, _region, _key)
+                .Build();
+
+            var result = await azureKernel.InvokePromptAsync(_prompt);
+
+            Console.WriteLine(result);
+
+        }
+        public async Task OpenAIChatStreaming(OpenAIPromptExecutionSettings options)
+        {
+            Kernel openAIKernel = Kernel.CreateBuilder()
+                .AddOpenAIChatCompletion(_model, _key)
+                .Build();
+
+            string fullMessage = string.Empty;
+            await foreach (var result in openAIKernel.InvokePromptStreamingAsync<StreamingChatMessageContent>(_prompt))
+            {
+                if (result.Content is { Length: > 0 })
+                {
+                    fullMessage += result.Content;
+                    Console.Write(result.Content);
+                }
+            }
+        }
+        public async Task AzureAIChatStreaming(OpenAIPromptExecutionSettings options)
+        {
+            Kernel azureKernel = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(_model, _region, _key)
+                .Build();
+
+            string fullMessage = string.Empty;
+            await foreach (var result in azureKernel.InvokePromptStreamingAsync<StreamingChatMessageContent>(_prompt))
+            {
+                if (result.Content is { Length: > 0 })
+                {
+                    fullMessage += result.Content;
+                    Console.Write(result.Content);
+                }
+            }
+
+        }
+    }
+}
