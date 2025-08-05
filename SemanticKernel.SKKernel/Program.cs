a65ffa7bad43d20d1b0d4ade9527672a8903cae2
Ather <atherrehan@live.com>
2025-08-05 11:31:42 +0400

    Environment variable set Sample prompt to verify

---
+++ SemanticKernel.SKKernel/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SemanticKernel;
+
+var openAIKey = Environment.GetEnvironmentVariable("SemanticKernelOpenAIKey",EnvironmentVariableTarget.User);
+//var azureRegion = Environment.GetEnvironmentVariable("SemanticKernelAzureRegion");
+//var azureKey = Environment.GetEnvironmentVariable("SemanticKernelAzureKey");
+
+Kernel openAIKernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion("gpt-4o-mini-2024-07-18", $"{openAIKey}")
+    .Build();
+
+
+//Kernel azureKernel = Kernel.CreateBuilder()
+//    .AddAzureOpenAIChatCompletion
+//    (
+//    "gpt-4o-mini",
+//    $"{azureRegion}",
+//    $"{azureKey}")
+//    .Build();
+
+
+var prompt = "What is Semantic Kernel, describe briefly";//What whaterver you want LLM to return
+
+var result = await openAIKernel.InvokePromptAsync(prompt);
+
+Console.WriteLine(result);
